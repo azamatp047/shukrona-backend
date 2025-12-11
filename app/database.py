b05ccabd2d7213f32ae9fb3c1a692df017ed4c85
Azamat Pulatov <azamatpulatov047@gmail.com>
2025-12-11 15:20:46 +0500

    changes datebase.py

--- app/database.py
+++ app/database.py
@@ -1,12 +1,28 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from app.config import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_HOST, POSTGRES_PORT
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+# Localda .env bor, productionda yo'q
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Railway DATABASE_URL → to‘g‘ri keladi
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Agar DATABASE_URL topilmasa, localga tushadi
+if not DATABASE_URL:
+    POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
+    POSTGRES_DB = os.getenv("POSTGRES_DB", "postgres")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
 
 def get_db():
