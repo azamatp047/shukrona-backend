c703197d8265fdb00d72db552cd75b949d1fb27a
Azamat Pulatov <azamatpulatov047@gmail.com>
2025-12-24 16:30:17 +0500

    remover star

--- app/main.py
+++ app/main.py
@@ -87,7 +87,6 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://shukrona-admin-panel.vercel.app",
-        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
